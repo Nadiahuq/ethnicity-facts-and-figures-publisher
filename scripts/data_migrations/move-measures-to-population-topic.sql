690871a2bec668f608e87ab98327d42c058b6168
Samuel Williams <samuelhwilliams@gmail.com>
2018-07-23 11:00:32 +0100

    Create migration to move measures
     ## Summary
    After the migration creating the population topic, we also need to move
    existing measures from their current subtopic to their new parents under
    population.
    
     ## Ticket
    https://trello.com/c/AhgrGiIL/857

---
+++ scripts/data_migrations/move-measures-to-population-topic.sql
@@ -0,0 +1,9 @@
+UPDATE page SET parent_guid = 'subtopic_national_and_regional_populations' WHERE guid = 'CXT 001' AND uri = 'population-of-england-and-wales-by-ethnicity';
+UPDATE page SET parent_guid = 'subtopic_national_and_regional_populations' WHERE guid = 'CXT 003' AND uri = 'ethnic-groups-by-region';
+
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE guid = 'RDU 006' AND uri = 'english-language-skills';
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE guid = 'CXT 004' AND uri = 'ethnic-groups-in-england-and-wales-by-sex';
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE guid = '738f4bf2-5eac-4820-adac-a5620f760b63' AND uri = 'working-age-population-of-england-and-wales';
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE guid = 'CXT 008' AND uri = 'lgbt-population-of-england-and-wales-by-ethnicity';
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE guid = 'CXT 005' AND uri = 'population-of-england-and-wales-by-ethnicity-and-socio-economic-group';
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE guid = 'RDU 001' AND uri = 'people-living-in-deprived-neighbourhoods';
